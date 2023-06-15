e2cebba4d3db142a605b2f97de73a339c3832aae
Ranu Yulianto <ranu.yulianto@bps.go.id>
2023-06-15 15:05:12 +0700

    Perbaikan lagi

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 gsap.timeline()
-    .from('.main-intro', { scale: 1.5, duration: 1 })
+    .from('.main-intro', { scale: 1.5, duration: 2 })
     .from('#face-text', { opacity: 0, duration: .1 })
     .from('#face-text', { duration: 1.5, y: 100 })
 // gsap.timeline({
@@ -42,8 +42,8 @@ var tl = gsap.timeline(
 tl.from('#row1 path', { duration: 2, x: "25vw", stagger: 0.1 }, 'enter')
 tl.from('#row2 path', { duration: 2, x: "-25vw", stagger: 0.1 }, 'enter')
 tl.from('#row3 > *', { duration: 2, x: "25vw", stagger: 0.1 }, 'enter')
-tl.fromTo('path.gray', { filter: "grayscale(0%) opacity(100%)" }, { duration: 2, filter: "grayscale(100%) opacity(15%)" })
-tl.fromTo('#text-sdgs', { opacity: 0 }, { duration: 3, opacity: 1 })
+tl.fromTo('path.gray', { filter: "grayscale(0%) opacity(100%)" }, { duration: 2, filter: "grayscale(100%) opacity(5%)" })
+tl.fromTo('#text-sdgs', { opacity: 0 }, { duration: .5, opacity: 1 })
 
 var gkTl = gsap.timeline({
     scrollTrigger: {
@@ -63,18 +63,18 @@ gkTl.set('#gkText2', { opacity: 1 }, 'text2Enter')
 gkTl.from('#gkText2', { y: '-50vh' })
 gkTl.add('air')
 gkTl.to('#air', { color: 'orange' }, 'air')
-gkTl.from('#gambar-air', { y: '50vh' }, 'air')
+// gkTl.from('#gambar-air', { y: '50vh' }, 'air')
 gkTl.add('listrik')
 gkTl.to('#air', { color: 'black' }, 'listrik')
 gkTl.to('#listrik', { color: 'orange' }, 'listrik')
-gkTl.to('#gambar-air', { x: '50vh' }, 'listrik')
-gkTl.from('#gambar-listrik', { y: '50vh' }, 'listrik')
+// gkTl.to('#gambar-air', { x: '50vh' }, 'listrik')
+// gkTl.from('#gambar-listrik', { y: '50vh' }, 'listrik')
 gkTl.add('makan')
 gkTl.to('#listrik', { color: 'black' }, 'makan')
 gkTl.to('#makan', { color: 'orange' }, 'makan')
-gkTl.to('#gambar-listrik', { x: '50vh' }, 'makan')
-gkTl.from('#gambar-makan', { y: '50vh' }, 'makan')
-gkTl.set('#makan', { delay: 0.1 })
+// gkTl.to('#gambar-listrik', { x: '50vh' }, 'makan')
+// gkTl.from('#gambar-makan', { y: '50vh' }, 'makan')
+// gkTl.set('#makan', { delay: 0.1 })
 
 gsap.timeline({
     scrollTrigger:
@@ -110,25 +110,25 @@ function getPeopleTrigger(num) {
 
 var peopleTimeline1 = gsap.timeline(getPeopleTrigger(2))
 peopleTimeline1.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
-peopleTimeline1.to('g.moneter, g.both', { fill: 'orange', 'fill-opacity': 1 }, 'change')
+peopleTimeline1.to('g.moneter, g.both, g.multi', { fill: 'gray', 'fill-opacity': 1 }, 'change')
 
 var peopleTimeline2 = gsap.timeline(getPeopleTrigger(3))
-peopleTimeline2.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
-peopleTimeline2.to('g.multi, g.both', { fill: 'gray', 'fill-opacity': 1 }, 'change')
+// peopleTimeline2.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
+peopleTimeline2.to('g.moneter, g.both', { fill: 'orange', 'fill-opacity': 1 }, 'change')
 
 var peopleTimeline2 = gsap.timeline(getPeopleTrigger(4))
-peopleTimeline2.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
-peopleTimeline2.to('g.both', { fill: 'red', 'fill-opacity': 1 }, 'change')
+peopleTimeline2.to('#people g.moneter', { fill: 'gray', 'fill-opacity': 1 }, 'change')
+peopleTimeline2.to('g.multi, g.both', { fill: 'blue', 'fill-opacity': 1 }, 'change')
 
 var peopleTimeline2 = gsap.timeline(getPeopleTrigger(5))
-peopleTimeline2.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
-peopleTimeline2.to('g.multi', { fill: 'gray', 'fill-opacity': 1 }, 'change')
+peopleTimeline2.to('#people g.multi', { fill: 'gray', 'fill-opacity': 1 }, 'change')
+peopleTimeline2.to('g.both', { fill: 'red', 'fill-opacity': 1 }, 'change')
 
 var peopleTimeline2 = gsap.timeline(getPeopleTrigger(6))
-peopleTimeline2.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
-peopleTimeline2.to('g.both', { fill: 'red', 'fill-opacity': 1 }, 'change')
-peopleTimeline2.to('g.moneter', { fill: 'orange', 'fill-opacity': 1 }, 'change')
-peopleTimeline2.to('g.multi', { fill: 'gray', 'fill-opacity': 1 }, 'change')
+// peopleTimeline2.to('#people g', { fill: 'gray', 'fill-opacity': .1 }, 'change')
+peopleTimeline2.to('g.both', { fill: 'gray', 'fill-opacity': 1 }, 'change')
+// peopleTimeline2.to('g.moneter', { fill: 'orange', 'fill-opacity': 1 }, 'change')
+peopleTimeline2.to('g.multi', { fill: 'green', 'fill-opacity': 1 }, 'change')
 
 // PIE SECTION
 gsap.timeline({
